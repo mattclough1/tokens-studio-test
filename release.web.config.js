83d3224409986303df26e2ea43489c1748ec611f
Matt Clough <cluffpost@gmail.com>
2023-05-02 11:18:43 -0400

    feat(web): random commit

--- release.web.config.js
+++ release.web.config.js
@@ -1,13 +1,22 @@
 module.exports = {
   branches: ["main"],
+  tagFormat: "web-${version}",
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [{ scope: "web", type: "fix", release: "patch" }],
+        releaseRules: [{ scope: "web", type: "feat", release: "minor" }],
+        releaseRules: [{ scope: "web", type: "*!", release: "major" }],
+      },
+    ],
     [
       "@semantic-release/npm",
       {
-        pkgRoot: "web/",
+        pkgRoot: "./web/",
         npmPublish: false,
       },
     ],
+    "@semantic-release/git",
   ],
 };
