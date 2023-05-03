5180f768eccc30354e6d9a37a306e9bbfe23a375
Matt Clough <cluffpost@gmail.com>
2023-05-03 09:19:22 -0400

    use base config for web release

--- web/release.base.config.js
+++ web/release.base.config.js
@@ -9,5 +9,25 @@ module.exports = {
         releaseRules: [{ scope: "!(web)", type: "*", release: false }],
       },
     ],
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/git",
+      {
+        message:
+          "chore(web): release <%= nextRelease.version %> - <%= new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' }) %> [skip ci]\n\n<%= nextRelease.notes %>",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "./web/",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "web/CHANGELOG.md",
+      },
+    ],
   ],
 };
